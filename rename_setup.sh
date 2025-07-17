f3fd4537cab3117ca04ab099ee5875140d7ceb88
Jeremy Delannoy <jeremydelannoy.jd@gmail.com>
2025-07-17 11:16:38 +0200

    Delete rename_setup

--- rename_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Move contents from Mac-Setup to Setup
-git mv Mac-Setup Setup
-
-# Commit changes
-git commit -m "refactor: rename Mac-Setup to Setup"
-
-echo "✅ Renamed Mac-Setup to Setup on develop branch"
